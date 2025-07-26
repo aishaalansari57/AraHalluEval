680ede5abf30914893bac70610a9d6c23432c27b
Aisha Alansari <aishaalansari22@gmail.com>
2025-07-26 19:01:04 +0300

    models.py

--- models.py
+++ models.py
@@ -5,10 +5,18 @@ MODEL_MAP = {
     "Jais-1.3B": "inceptionai/jais-family-1p3b",
     "Jais-2.7b": "inceptionai/jais-family-2p7b",
     "Jais-6.7b": "inceptionai/jais-family-6p7b",
-    "Noon-7b": "aseej/noon-7b",
+    "Noon": "Naseej/noon-7b",
     "Allam": "ALLaM-AI/ALLaM-7B-Instruct-preview",
-    "Fanar-1.9B": "QCRI/Fanar-1-9B",  # use the exact HF path if different
-    "Gemma-3.4B": "google/gemma-3-4b-pt"
+    "Fanar": "QCRI/Fanar-1-9B",  # use the exact HF path if different
+    "Gemma": "google/gemma-3-4b-pt",
+    "llama":"meta-llama/Meta-Llama-3-8B",
+    "qwen2.5-1.5b":"Qwen/Qwen2.5-1.5B",
+    "qwen2.5-3b":"Qwen/Qwen2.5-3B",
+    "qwen2.5-7b":"Qwen/Qwen2.5-7B",
+    "bloom-1.7b":"bigscience/bloom-1b7",
+    "bloom-":"bigscience/bloom-3b",
+    "bloom-":"bigscience/bloom-7b",
+
 }
 
 def load_model_and_tokenizer(model_key):
